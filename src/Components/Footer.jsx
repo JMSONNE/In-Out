fba06fb79358abd491efaed65644210a7aaa2f97
JMSONNE <jordynmsonne@gmail.com>
2024-09-20 15:27:46 -0500

    made navbar responsive & added footer text

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
     return (
         <>
             <footer className='footer'>
-                <div className='footer-text'>Footer</div>
+                <div className='footer-text'>© 2024 - In & Out Cuts</div>
             </footer>
         </>
     )
