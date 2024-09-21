588103febfe48ad7f249387e2a5b1146005422fc
JMSONNE <jordynmsonne@gmail.com>
2024-09-20 21:55:03 -0500

    added avatar photos and changed pricing to be accurate

--- src/Components/Pricing.jsx
+++ src/Components/Pricing.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import logo from '../assets/logo.jpg'
+import tessapic from '../assets/tessapic.jpg'
+import tammypic from '../assets/tammypic.jpg'
+import katiepic from '../assets/katiepic.jpg'
 
 export const Pricing = () => {
     return (
@@ -10,7 +13,7 @@ export const Pricing = () => {
                     <h2 className="stylist-name">Tessa</h2>
                     <div className="flex-wrapper">
                         <img
-                            src={logo}
+                            src={tessapic}
                             alt="Tessa"
                             className="avatar" />
                         <ul className='stylist-pricing'>
@@ -30,17 +33,17 @@ export const Pricing = () => {
                     <h2 className="stylist-name">Tammy</h2>
                     <div className="flex-wrapper">
                         <img
-                            src={logo}
+                            src={tammypic}
                             alt="Tammy"
                             className="avatar" />
                         <ul className='stylist-pricing'>
                             <li>Kids - $15.00 and up</li>
                             <li>Men's - $20.00 and up</li>
                             <li>Women's - $20.00 and up</li>
-                            <li>Senior's - $15.00 and up</li>
-                            <li>Perms - $70.00 and up</li>
-                            <li>Highlights partial - $100</li>
-                            <li>Highlights - $140 and up</li>
+                            <li>Partial Highlights - $100</li>
+                            <li>Highlights - $130 and up</li>
+                            <li>Color - $95 and up</li>
+
                         </ul>
                     </div>
                 </section>
@@ -50,7 +53,7 @@ export const Pricing = () => {
                     <h2 className="stylist-name">Katie</h2>
                     <div className="flex-wrapper">
                         <img
-                            src={logo}
+                            src={katiepic}
                             alt="Katie"
                             className="avatar" />
                         <ul className='stylist-pricing'>
@@ -58,9 +61,6 @@ export const Pricing = () => {
                             <li>Men's - $20.00 and up</li>
                             <li>Women's - $20.00 and up</li>
                             <li>Senior's - $15.00 and up</li>
-                            <li>Perms - $70.00 and up</li>
-                            <li>Highlights partial - $100</li>
-                            <li>Highlights - $140 and up</li>
                         </ul>
                     </div>
                 </section>
