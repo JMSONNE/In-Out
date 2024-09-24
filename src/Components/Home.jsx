53130c60855df910581b898a538d83ae9b36f57e
JMSONNE <jordynmsonne@gmail.com>
2024-09-23 20:38:52 -0500

    added about us content and layout

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,6 +9,16 @@ const Home = () => {
                 <div className="about-us-banner">
                     <h1 className="about-us-banner-text">About us</h1>
                 </div>
+
+                {/* About us content */}
+                <div className='about-us-content'>
+                    <img src="src\assets\image.png"
+                        alt=""
+                        className="homepage-image"
+                    />
+                    <p className="about-us-text">asdfasdfasdfasdfasdfasdfasdfasdfasdfasdf</p>
+                </div>
+
             </section>
         </>
     )
