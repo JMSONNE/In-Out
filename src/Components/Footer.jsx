f64e7171dbdd1af957ba187a70a6093773d54ca5
JMSONNE <jordynmsonne@gmail.com>
2024-09-23 19:25:04 -0500

    added facebook icon and link to footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import { IconButton } from '@mui/material';
 
 export const Footer = () => {
+
+    // Handle social media icons
+    const facebook = "https://www.facebook.com/profile.php?id=100054282081055";
+
     return (
         <>
             <footer className='footer'>
-                <div className='footer-text'>© 2024 - In & Out Cuts</div>
+                <div className="footer-content">
+                    <div className="copyright">
+                        <div className='footer-text'>© 2024 - In & Out Cuts</div>
+                    </div>
+                    <div className="social-icons">
+                        <IconButton
+                            href={facebook}>
+                            <FacebookIcon fontSize='large' />
+                        </IconButton>
+                    </div>
+                </div>
             </footer>
         </>
     )
