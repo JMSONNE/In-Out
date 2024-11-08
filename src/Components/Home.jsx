a7b5d363a291611a1d0207f1312c489f4dd878b5
JMSONNE <jordynmsonne@gmail.com>
2024-11-08 15:32:06 -0600

    added award content

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -17,7 +17,10 @@ const Home = () => {
                         className="homepage-image"
                     />
                     <p className="about-us-text">
-                        Walk-ins welcome, come see us for all of your hair care needs. Just ask for Tammy, Tessa, katie or Andrea.
+                        Welcome to In & Out Cuts, your local spot for stylish cuts, vibrant colors, and personalized service!
+                        Located in the heart of Springtown, we’re passionate about making you look and feel your best.
+                        Our talented team combines small-town warmth with the latest trends, offering a friendly,
+                        relaxing environment where everyone feels at home. Come in for a fresh look – we can’t wait to make your hair dreams a reality!
                     </p>
                 </div>
 
@@ -28,11 +31,16 @@ const Home = () => {
 
                 {/* Awards content */}
                 <div className='award-content'>
-                    {/* <img src="" alt="" /> */}
+
+                    <img
+                        className='homepage-image'
+                        src="https://scontent-dfw5-1.xx.fbcdn.net/v/t39.30808-6/217635115_4459259420752999_2850506802059342367_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=Alxj59vQyBEQ7kNvgExxYun&_nc_zt=23&_nc_ht=scontent-dfw5-1.xx&_nc_gid=AlUDjNDax3yo08iyvpidWzH&oh=00_AYA2URXSJ_vdHk6xszvdn6aD0yK1o_cAKNHiFvXXT4LtWQ&oe=67343AB4"
+                        alt="Award photo" />
+
                     <ul className='award-list' type="square">
-                        <li>award date</li>
-                        <li>award date</li>
-                        <li>award date</li>
+                        <li>2021 Best of Parker County</li>
+                        <li>2020 Best of Parker County</li>
+                        <li>2018 Best of Parker County</li>
                     </ul>
                 </div>
 
